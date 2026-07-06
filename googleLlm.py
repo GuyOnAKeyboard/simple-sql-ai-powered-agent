41a4e64800fcae0778985dea3179e315965d2b0f
unknown <Administrator@BG4NB1284.SONATA.LOCAL>
2026-07-06 22:32:57 +0530

    deleting extra file

--- googleLlm.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-
-
-if __name__=="__main__":
-    response = google_llm.invoke("What is tool calling in langchain?")
-    print("\nResponse Content: ", response.content)
